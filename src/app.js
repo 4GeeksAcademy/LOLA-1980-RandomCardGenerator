6bfe056b2bdd54c9ff69d0ffe8a257282a92a2ed
LOLA-1980 <109870340+LOLA-1980@users.noreply.github.com>
2024-03-25 22:43:57 +0000

    [Se hizo el generador de cards, y los 3 puntos siguientes: 1.-Agregar el boton que genera una nueva cards 2.-Agregar el timer de 10 segundos 3.- Y los inputs con ancho y altura]

--- src/app.js
+++ src/app.js
@@ -7,5 +7,53 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  function elementRandom(elements) {
+    let randomIndex = Math.floor(Math.random() * elements.length);
+    return elements[randomIndex];
+  }
+
+  function updateCard() {
+    // Generar números y palos por defecto al inicio
+    document.getElementById("numero").innerHTML = elementRandom(numeros);
+    let palo = document.getElementById("palo");
+    let palo1 = document.getElementById("palo1");
+    let paloAleatorio = elementRandom(palos);
+    if (paloAleatorio === "♥" || paloAleatorio === "♦") {
+      palo.style.color = "red";
+      palo1.style.color = "red";
+      palo1.style.transform = "rotate(180deg)";
+    }
+    palo.innerHTML = paloAleatorio;
+    palo1.innerHTML = paloAleatorio;
+  }
+
+  //Llamar a la funcion updateCard cada 10 segundos
+  setInterval(updateCard, 10000);
+
+  //Event listener para el boton
+  document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("numero").innerHTML = elementRandom(numeros);
+
+    let palo = document.getElementById("palo");
+    let palo1 = document.getElementById("palo1");
+    let paloAleatorio = elementRandom(palos);
+    if (paloAleatorio === "♥" || paloAleatorio === "♦") {
+      palo.style.color = "red";
+      palo1.style.color = "red";
+      palo1.style.transform = "rotate(180deg)";
+    }
+    palo.innerHTML = paloAleatorio;
+    palo1.innerHTML = paloAleatorio;
+
+    //Obtener los valores de ancho y alto ingresados por el usuario
+    let widthInput = document.getElementById("widthInput").value;
+    let heightInput = document.getElementById("heightInput").value;
+
+    //Aplicar los valores de ancho y alto a la tarjeta
+    document.getElementById("card").style.width = widthInput + "rem";
+    document.getElementById("card").style.height = heightInput + "rem";
+  });
 };
